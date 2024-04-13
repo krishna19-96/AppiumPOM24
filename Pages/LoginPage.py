4ea7455126d43be0ffabd977a6ee6427ea135982
Krishna <52797146+krishna19-96@users.noreply.github.com>
2024-04-13 22:27:52 +0530

    Update LoginPage.py
    add time.sleep before mobile number field

--- Pages/LoginPage.py
+++ Pages/LoginPage.py
@@ -14,6 +14,7 @@ class LoginPage(BasePage):
         self.click("LocationPermission_XPATH")
         time.sleep(3)
         self.click("NotificationPermission_ID")
+        time.sleep(2)
         for lis in MobileNumber:
             self.type("MobileNumber_ID", lis)
         self.click("MobileNumber_ID")
